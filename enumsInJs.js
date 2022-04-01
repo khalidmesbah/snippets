0f3bde711a94c5af50aec4d283fb100fb61d6c79
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-04-01 19:09:15 +0200

    up

---
+++ enumsInJs.js
@@ -0,0 +1,11 @@
+const Game_Stats = Object.freeze({
+  NOT_STARTED: "Not Started",
+  PLAYING: "Playing",
+  FINISHED: "finished",
+});
+
+console.log(Game_Stats);
+
+let gameState = Game_Stats.NOT_STARTED;
+gameState = Game_Stats.PLAYING;
+gameState = Game_Stats.FINISHED;
