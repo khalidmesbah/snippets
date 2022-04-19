d5893c470419440c8703c493f1c1c44793c65eea
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-04-19 03:17:48 +0200

    up

--- console.js
+++ console.js
@@ -1,57 +1,87 @@
-console.log(`hello`);
-console.error(`hello`);
-console.warn(`hello`);
-console.assert(`hello`);
-console.assert(0 || `error`);
-console.dir("hello");
+console.log(
+  "string: %s, number: %i, float: %f, object: %o",
+  "khalid",
+  21,
+  21.5,
+  { name: "khalid", age: 21 }
+);
+console.log(
+  `%c javascript is awesome with styling`,
+  `color:red;background:black;font-size:30px`
+);
+console.log(`log`);
+console.error(`error`);
+console.warn(`warn`);
+console.debug("debug");
+console.info("info");
+console.assert(`assert`);
+console.assert(true || `true`);
+if (true) console.error("true");
+console.assert(false || `false`);
+if (  false) console.error("false");
+console.dir("dir");
 console.table([
-  ["foo", 21],
-  ["bar", 14],
-  ["loda", 32],
-  ["khaled", 32],
-  ["mesbah", 32],
+  ["table", 1],
+  ["table", 2],
+  ["table", 3],
+  ["table", 4],
+  ["table", 5],
 ]);
 const obj = {
-  name: "khaled",
-  salary: "unknown",
+  name: "name",
+  salary: "salary",
 };
 console.table(obj);
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
 console.log(`====================`);
-console.countReset("loda");
+console.group(`outside group`);
+console.group(`inside group`);
+console.log(`inside inside group`);
+console.groupEnd();
+console.groupEnd();
 console.log(`====================`);
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
-console.count("loda");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.countReset("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count("count");
+console.count()
+console.count()
+console.log(`====================`);
 
 /* time */
-console.time("hi");
-// console.timeStamp("hi");
-setTimeout((e) => {
-  return 1;
-}, 100);
-console.timeLog("hi");
-console.timeEnd("hi");
+console.time("time");
+console.timeLog("time");
+for (let i = 0; i < 1000000; i++);
+console.timeLog("time");
+for (let i = 0; i < 1000000; i++);
+console.timeEnd("time");
+
+/* time 2 => will appear in the javascript profiler in the web console / dev tools */
+console.profile("time");
+for (let i = 0; i < 1000000; i++);
+console.timeStamp("time");
+for (let i = 0; i < 1000000; i++);
+console.profileEnd("time");
 
 /* trace */
 console.log(`======trace======`);
@@ -64,8 +94,3 @@ function fun1() {
 }
 fun1();
 console.log(`======end trace======`);
-
-console.log(
-  `%c javascript is awesome `,
-  `color:red;background:black;font-size:30px`
-);
